bb6a57fd4f475d84e7858cbbdaaeed22c4046498
WhiteMask <1049806421@qq.com>
2020-04-19 14:57:08 +0800

    Update and rename 栈（链表实现）.c to 栈和队列（链表实现）.c

---
+++ 栈和队列（链表实现）.c
@@ -0,0 +1,104 @@
+//栈的创建
+#pragma warning(disable:4996)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node * List;
+
+struct Node
+{
+	int number;
+	List next;
+};
+
+List Create(int n);
+
+int main()
+{
+	List list;
+	int n;
+	scanf("%d",&n);
+	list = Create(n);
+	while (list)
+	{
+		printf("%d",list->number);
+		list = list->next;
+	}
+}
+
+List Create(int n)
+{
+	List L, t,cycle;
+	L = (List)malloc(sizeof(struct Node));
+	L->next = NULL;
+	t = L;
+	for (int i=0;i<n;i++)
+	{
+		List temp;
+		temp = (List)malloc(sizeof(struct Node));
+		temp->next = NULL;
+		int number;
+		scanf("%d",&number);
+		temp->number = number;
+		temp->next = t->next;
+		t->next = temp;
+	}
+	cycle = L;
+	L = L->next;
+	free(cycle);
+	return L;
+}
+
+//队列的创建
+#pragma warning(disable:4996)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node * List;
+
+struct Node
+{
+	int number;
+	List next;
+};
+
+List Create(int n);
+
+int main()
+{
+	List list;
+	int n;
+	scanf("%d",&n);
+	list = Create(n);
+	while (list)
+	{
+		printf("%d",list->number);
+		list = list->next;
+	}
+}
+
+List Create(int n)
+{
+	List L, t,cycle;
+	L = (List)malloc(sizeof(struct Node));
+	L->next = NULL;
+	t = L;
+	for (int i=0;i<n;i++)
+	{
+		List temp;
+		temp = (List)malloc(sizeof(struct Node));
+		temp->next = NULL;
+		int number;
+		scanf("%d",&number);
+		temp->number = number;
+		t->next = temp;
+		t = temp;
+	}
+	cycle = L;
+	L = L->next;
+	free(cycle);
+	return L;
+}
+
