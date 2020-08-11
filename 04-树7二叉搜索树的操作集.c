2ac349bb68683b6952289609e7a08bf27ee979ac
WhiteMask <1049806421@qq.com>
2020-08-11 21:27:49 +0800

    Update 04-树7二叉搜索树的操作集.c

--- 04-树7二叉搜索树的操作集.c
+++ 04-树7二叉搜索树的操作集.c
@@ -23,6 +23,35 @@ BinTree Insert(BinTree BST, ElementType X)
 	return BST;
 }
 
+
+Position FindMin(BinTree BST)
+{
+	BinTree t;
+	t = BST;
+	if (t)
+	{
+	while (t->Left)
+	{
+		t = t->Left;
+	}
+	}
+
+	return t;
+}
+
+Position FindMax(BinTree BST)
+{
+	BinTree t;
+	t = BST;
+	if(t)
+	{
+		while (t->Right)
+	{
+		t = t->Right;
+	} 
+	}
+	return t;
+}
 Position Find(BinTree BST, ElementType X) {
 	if (!BST)
 	{
@@ -40,6 +69,39 @@ Position Find(BinTree BST, ElementType X) {
     {
 	    return BST;
     }
+}
+BinTree Delete(BinTree BST, ElementType X) {
+	BinTree p;
+	if (!BST) {
+		printf("Not Found\n");
+		return BST;
+	}
+	if (X < BST->Data) {
+		BST->Left = Delete(BST->Left, X);
+	}
+	else if (X > BST->Data) {
+		BST->Right = Delete(BST->Right, X);
+	}
+	else {
+		if (BST->Left && BST->Right) {
+			p = FindMax(BST->Left);
+			BST->Data = p->Data;
+			BST->Left = Delete(BST->Left, BST->Data);
+		}
+		else {
+			p = BST;
+			if (!BST->Left) {
+				BST = BST->Right;
+			}
+			else if (!BST->Right) {
+				BST = BST->Left;
+			}
+			free(p);
+		}
+	}
+	return BST;
+}
     /*
     用递归算法的时候，务必不要用新的结点指代形参。
+    
     */
