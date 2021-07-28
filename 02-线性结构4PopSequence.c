2d258f7dbb73186f08182193291144f06fd8c9a6
WhiteMask <1049806421@qq.com>
2021-07-28 18:33:09 +0800

    Update 02-线性结构4PopSequence.c

--- 02-线性结构4PopSequence.c
+++ 02-线性结构4PopSequence.c
@@ -1,5 +1,3 @@
-//未写完（10/25）
-//测试点0 sample乱序，一般的Y&N 未通过
 #pragma warning(disable:4996)
 
 #include <stdio.h>
@@ -23,22 +21,22 @@ int Is(List S,List check,int M);//判断函数
 int main()
 {
 	int M, N, K;//M是最大容量 N是长度 K是需要检查几个表
-	scanf("%d %d %d",&M,&N,&K);
-	if (M<0||N<0||K<0)    //如果输入的数字为负数，则结束程序
+	scanf("%d %d %d", &M, &N, &K);
+	if (M < 0 || N < 0 || K < 0)    //如果输入的数字为负数，则结束程序
 	{
 		return 0;
 	}
-	int i,result;
+	int i, result;
 	i = 0;
 	result = 0;
 	List check;
-	check = Input1();  //用来做判断的栈（先入后出）
-	for (i=0;i<K;i++)  //一一判断输入的数字组是否符合要求
+	for (i = 0; i < K; i++)  //一一判断输入的数字组是否符合要求
 	{
-		List S1;   
+		check = Input1();  //用来做判断的栈
+		List S1;
 		S1 = Input(N); //输入的数字组以队列的方式储存（非循环队列）
-		result = Is( S1,check,M); //判断结果 1为yes 0为no
-		if (result==1)
+		result = Is(S1, check, M); //判断结果 1为yes 0为no
+		if (result == 1)
 		{
 			printf("YES\n");
 		}
@@ -120,45 +118,44 @@ List Input1( )//栈的输入
 
 int Is(List S1, List check,int M)  //判断
 {
-	int number;
+int number;
 	int number1;
 	int number2;
 	int count;
-	int flag;
-	flag = 0;
 	count = 1;
 	number = 1;
 	number2 = 0;
 	number1 = 0;
-	List S,check1;
+	List S, check1;
 	S = S1;
 	check1 = check;
-
+	push(1, check1);
 	while (S)
 	{
 		if (!S)
 		{
 			return 0;
 		}
-		if (flag==0)
-		{
-			flag = 1;
-			push(1,check1);
-		}  //第一次执行 在空栈里压入1
 		number1 = S->number;  //输入的数字组的元素
 		number2 = pop(check1);//栈的元素
-		if (number2==number1)
+		if (number2 == number1)
 		{
 			S = S->next;
 			count--; //数字一样出栈
+			if (count == 0)
+			{
+				count++;
+				number++;
+				push(number,check1);
+			}
 		}
-		else if(number2<number1)
+		else if (number2 < number1)
 		{
 			push(number2, check1);
 			number++;
 			push(number, check1);//数字不一样，把之前的数字入栈，同时压入一个新的数字
 			count++;             //已有的数字个数+1
-			if (count>M||count<0) //判断栈的容量是否溢出
+			if (count > M || count < 0) //判断栈的容量是否溢出
 			{
 				return 0;
 			}
@@ -168,6 +165,6 @@ int Is(List S1, List check,int M)  //判断
 			return 0;
 		}
 	}
-	
+
 	return 1;
 }
